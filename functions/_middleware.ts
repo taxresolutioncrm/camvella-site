4c971dc4c9b3dceef4d8aa8fd8cfb4e2ad464abc
RomyLabs <rcruz187@gmail.com>
2026-09-18 11:52:23 -0700

    Sandbox SEO: collapse canonical redirects to one hop

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -9,13 +9,6 @@ export async function onRequest(context: PagesContext): Promise<Response> {
   const url = new URL(context.request.url)
   const needsHttps = url.protocol !== 'https:'
   const needsApex = url.hostname === `www.${CANONICAL_HOST}`
-
-  if (needsHttps || needsApex) {
-    url.protocol = 'https:'
-    url.hostname = CANONICAL_HOST
-    return Response.redirect(url.toString(), 301)
-  }
-
   const regionalAliases: Record<string, string> = {
     '/solutions/florida-hoa-management-software': '/locations/florida/',
     '/solutions/florida-hoa-management-software/': '/locations/florida/',
@@ -23,10 +16,15 @@ export async function onRequest(context: PagesContext): Promise<Response> {
     '/solutions/texas-hoa-management-software/': '/locations/texas/',
   }
   const regionalCanonical = regionalAliases[url.pathname]
-  if (regionalCanonical) {
-    url.pathname = regionalCanonical
-    url.search = ''
-    url.hash = ''
+
+  if (needsHttps || needsApex || regionalCanonical) {
+    url.protocol = 'https:'
+    if (needsApex) url.hostname = CANONICAL_HOST
+    if (regionalCanonical) {
+      url.pathname = regionalCanonical
+      url.search = ''
+      url.hash = ''
+    }
     return Response.redirect(url.toString(), 301)
   }
 
