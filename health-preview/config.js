0ce7a5ba2e54386c3215f21f296923fd0aff4191
RomyLabs <rcruz187@gmail.com>
2026-09-18 15:33:02 -0700

    Add safe browser runtime config placeholder for Supabase handoff

---
+++ health-preview/config.js
@@ -0,0 +1,9 @@
+globalThis.__HEALTH_CRM_CONFIG__={
+  supabaseUrl:'',
+  supabasePublishableKey:'',
+  environment:'sandbox',
+  productName:'RomyLabs Insurance CRM',
+  appOrigin:location.origin,
+  publicIntakeSlug:'',
+  publicBookingSlug:''
+};
