8e70c6c3341ee1c4084f7a58a4fa4399741b32ed
RomyLabs <rcruz187@gmail.com>
2026-09-18 05:56:28 -0700

    Add browser runtime config example

---
+++ health-preview/runtime-config.example.js
@@ -0,0 +1,7 @@
+window.__HEALTH_CRM_CONFIG__={
+  environment:'sandbox',
+  productName:'RomyLabs Insurance CRM',
+  appOrigin:'https://APP_DOMAIN',
+  supabaseUrl:'https://PROJECT_REF.supabase.co',
+  supabasePublishableKey:'PUBLISHABLE_KEY'
+};
